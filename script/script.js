bfc7008f650947076c86d88ecacab0161b5f3434
im-boyo <felixtradovanovic@gmail.com>
2023-05-08 14:21:16 -0400

    rgu

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
 
 let anim = gsap.timeline().to(".scroll-down", {y: "10vmin", ease: "none", yoyo: true, repeat: -1, duration: 3})
 
@@ -31,7 +32,6 @@ window.addEventListener('scroll', function(){
 const animCh1 = gsap
 .timeline({
   scrollTrigger: {
-    markers: true,
     start: "center center",
     end: "bottom top",
     toggleActions: "restart complete reverse reset",
@@ -39,9 +39,9 @@ const animCh1 = gsap
     pin: true,
   }
 })
-.fromTo("#chapitre-1 .poisson", {x: "-20vw", opacity: 0}, {x: "0vw", opacity: 1, duration: 3, border: "5px solid green"})
-.fromTo("#chapitre-1 .billy1", {x: "-20vw", opacity: 0}, {x: "20vw", opacity: 1, duration: 3, border: "5px solid green"})
-.fromTo("#chapitre-1 .poisson", {x: "0vw", opacity: 1}, {x: "40vw", opacity: 0, duration: 3, border: "5px solid green"}, "<1")
+.fromTo("#chapitre-1 .poisson", {x: "-20vw", opacity: 0}, {x: "0vw", opacity: 1, duration: 3})
+.fromTo("#chapitre-1 .billy1", {x: "-20vw", opacity: 0}, {x: "20vw", opacity: 1, duration: 3})
+.fromTo("#chapitre-1 .poisson", {x: "0vw", opacity: 1}, {x: "40vw", opacity: 0, duration: 3}, "<1")
 
 
 
@@ -50,16 +50,17 @@ const animCh1 = gsap
 
 const animCh2 = gsap.timeline({
     scrollTrigger: {
-      markers: true,
-      start: "center center",
+        start: "center center",
       end: "bottom top",
       toggleActions: "restart complete reverse reset",
       trigger: "#chapitre-2",
       pin: true,
       }
   })
-.fromTo(".bulles1", {y: "30vh", opacity: "0%"}, {y: "0vh", opacity: "100%", duration: 3, border: "5px solid green"})
-
+.fromTo(".bulles1", {y: "30vh", opacity: "0%"}, {y: "0vh", opacity: "100%", duration: 3})
+.from(".coffre", {opacity: 0, duration: 3}, "<3")
+.from(".coffre_haut", {y: "15vh", duration: 3})
+.from(".medaillon", {y: "4vh", duration: 2}, "<")
 
 
 
@@ -68,16 +69,15 @@ const animCh2 = gsap.timeline({
 
 const animCh3 = gsap.timeline({
     scrollTrigger: {
-      markers: true,
-      start: "center center",
+        start: "center center",
       end: "bottom top",
       toggleActions: "restart complete reverse reset",
       trigger: "#chapitre-3",
       pin: true,
       }
   })
-.fromTo(".bulles2", {y: "10vh", opacity: "0%"}, {y: "0vh", opacity: "100%", duration: 3, border: "5px solid green"})
-.fromTo("#chapitre-3 .lola1", {x: "10vw", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 2, border: "5px solid green"}, "<1")
+.fromTo(".bulles2", {y: "10vh", opacity: "0%"}, {y: "0vh", opacity: "100%", duration: 3})
+.fromTo("#chapitre-3 .lola1", {x: "10vw", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 2}, "<1")
 
 
 
@@ -88,17 +88,16 @@ const animCh3 = gsap.timeline({
 
 const animCh4 = gsap.timeline({
     scrollTrigger: {
-      markers: true,
-      start: "center center",
+        start: "center center",
       end: "bottom top",
       toggleActions: "restart complete reverse reset",
       trigger: "#chapitre-4",
       pin: true,
       }
   })
-.from(".poisson-groupe", {x: "103vw", duration: 5, repeat: -1, ease: "none", border: "5px solid green"})
-.from(".billynage", {x: "-100vw", duration: 10, border: "5px solid green"}, "<3")
-.from(".lolanage", {x: "-100vw", duration: 10, border: "5px solid green"}, "<")
+.from(".poisson-groupe", {x: "103vw", duration: 5, repeat: -1, ease: "none"})
+.from(".billynage", {x: "-100vw", duration: 10}, "<3")
+.from(".lolanage", {x: "-100vw", duration: 10}, "<")
 
 
 
@@ -109,7 +108,6 @@ const animCh4 = gsap.timeline({
 gsap.to(".grotte", {
   scrollTrigger: {
     scrub: true,
-    markers: true,
     trigger: "#chapitre-5"
   },
   y: "-300",
@@ -119,7 +117,6 @@ gsap.to(".grotte", {
 gsap.to(".sableP", {
   scrollTrigger: {
     scrub: true,
-    markers: true,
   },
   y: "900",
   ease: "none"
@@ -128,7 +125,6 @@ gsap.to(".sableP", {
 gsap.to(".alguesavant", {
   scrollTrigger: {
     scrub: true,
-    markers: true,
     trigger: "#chapitre-5"
   },
   x: "2500",
@@ -139,7 +135,6 @@ gsap.to(".alguesavant", {
 gsap.to(".alguesarriere", {
   scrollTrigger: {
     scrub: true,
-    markers: true,
     trigger: "#chapitre-5"
   },
   x: "-1900",
@@ -150,7 +145,6 @@ gsap.to(".alguesarriere", {
 gsap.to(".meduses", {
   scrollTrigger: {
     scrub: true,
-    markers: true,
     trigger: "#chapitre-5"
   },
   y: "-900",
@@ -164,8 +158,7 @@ gsap.to(".meduses", {
 
 const animCh6 = gsap.timeline({
     scrollTrigger: {
-      markers: true,
-      start: "center center",
+        start: "center center",
       end: "bottom top",
       toggleActions: "restart complete reverse reset",
       trigger: "#chapitre-6",
@@ -173,22 +166,22 @@ const animCh6 = gsap.timeline({
       scrub: true
       }
   })
-.fromTo("#chapitre-6 .billy2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"})
-.fromTo(".p6-1", {opacity: 0}, {opacity: 1, border: "5px solid green"}, "<")
-.fromTo("#chapitre-6 .lola2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"}, "<")
-.fromTo("#chapitre-6 .medaillon", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"}, "<")
-.fromTo("#chapitre-6 .poisson-blue-queue", {x: "10%", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 3, border: "5px solid green"}, "<2")
-.fromTo("#chapitre-6 .poisson-blue-corps", {x: "10%", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 3, border: "5px solid green"}, "<")
-.fromTo("#chapitre-6 .billy2", {opacity: "100%", duration: 3, border: "5px solid green"}, {opacity: "0%"}, "<7")
+.fromTo("#chapitre-6 .billy2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3})
+.fromTo(".p6-1", {opacity: 0}, {opacity: 1}, "<")
+.fromTo("#chapitre-6 .lola2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3}, "<")
+.fromTo("#chapitre-6 .medaillon", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3}, "<")
+.fromTo("#chapitre-6 .poisson-blue-queue", {x: "10%", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 3}, "<2")
+.fromTo("#chapitre-6 .poisson-blue-corps", {x: "10%", opacity: "0%"}, {x: "0%", opacity: "100%", duration: 3}, "<")
+.fromTo("#chapitre-6 .billy2", {opacity: "100%", duration: 3}, {opacity: "0%"}, "<7")
 .fromTo(".p6-1", {opacity: 1}, {opacity: 0}, "<")
-.fromTo("#chapitre-6 .lola2", {opacity: "100%", duration: 3, border: "5px solid green"}, {opacity: "0%"}, "<")
+.fromTo("#chapitre-6 .lola2", {opacity: "100%", duration: 3}, {opacity: "0%"}, "<")
 .to("#chapitre-6 .poisson-blue-corps", {opacity: 0}, "<")
 .to("#chapitre-6 .poisson-blue-queue", {opacity: 0}, "<")
 .to("#chapitre-6 .medaillon", {opacity: 0}, "<")
-.fromTo(".billymains", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"})
+.fromTo(".billymains", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3})
 .fromTo(".p6-2", {opacity: 0}, {opacity: 1}, "<")
-.fromTo(".medaillon2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"}, "<")
-.fromTo([".poisson-blue-queue2",".poisson-blue-corps2"], {x: "10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3, border: "5px solid green"})
+.fromTo(".medaillon2", {x: "-10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3}, "<")
+.fromTo([".poisson-blue-queue2",".poisson-blue-corps2"], {x: "10vw", opacity: "0%"}, {x: "0vw", opacity: "100%", duration: 3})
 
 
 
@@ -199,13 +192,36 @@ const animCh6 = gsap.timeline({
 
 const animCh7 = gsap.timeline({
     scrollTrigger: {
-      markers: true,
-      start: "center center",
+        start: "center center",
       end: "bottom top",
       toggleActions: "restart complete reverse reset",
       trigger: "#chapitre-7",
       pin: true,
     }
   })
-.fromTo(["#chapitre-7 .billy2", "#chapitre-7 .lola1", "#chapitre-7 .poisson-blue-queue", "#chapitre-7 .poisson-blue-corps"], {opacity: "0%", border: "5px solid green"}, {opacity: "100%", duration: 3})
+.fromTo(["#chapitre-7 .billy2", "#chapitre-7 .lola1", "#chapitre-7 .poisson-blue-queue", "#chapitre-7 .poisson-blue-corps"], {opacity: "0%"}, {opacity: "100%", duration: 3})
 .from(["#chapitre-7 .billy2", "#chapitre-7 .lola1", "#chapitre-7 .poisson-blue-queue", "#chapitre-7 .poisson-blue-corps"], {y: "-10vh", rotation: "20deg", yoyo: true, repeat: -1, ease: "none", duration: 3}, "<")
+.from(".bi-star-fill", {opacity: 0})
+.from(".bi-star-fill", {
+  motionPath: {
+    align: ".cls-1",
+    path: ".cls-1",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5]  
+  },
+  duration: 5,
+}, "<")
+.fromTo(
+  ".cls-1",
+  { drawSVG: "0% 0%"},
+  {
+    drawSVG:"100% 0%", 
+    duration:1, 
+    ease:"power1.inOut"
+  }, "<"
+);
+
+
+
+
+
